3a752180f40052cca9ea157b45e69b93cdc6b29d
Manel-93 <manel.benni.sio@gmail.com>
2026-04-23 00:10:07 +0200

    Derniers ajustements frontutilisateur

--- FrontendApi/storefront/src/services/contact.js
+++
@@ -1,6 +0,0 @@
-import api from '../api/client';
-
-export const sendContact = (body) => api.post('/contact', body);
-
-export const sendChatbotMessage = (message, sessionId, options = {}) =>
-  api.post('/chatbot/message', { message, sessionId, ...options });
--- FrontendApi/storefront/src/services/homeCarousel.js
+++
@@ -1,5 +0,0 @@
-import api from '../api/client';
-
-export function fetchHomeCarousel() {
-  return api.get('/home-carousel');
-}
